360f35fd65e373b6ae1094fba0365a3a21d7e237
BaicanVlad <baican.vladadrian@yahoo.com>
2019-01-19 18:02:56 +0200

    'Fix'

--- src/webapp/src/pages/Jobs/JobsPage.js
+++ src/webapp/src/pages/Jobs/JobsPage.js
@@ -10,7 +10,7 @@ export default class JobsPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            jobs: [],
+            jobs: null,
             searchValue: "",
             userName: "TestUserName"
         }
