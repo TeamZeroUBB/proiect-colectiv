64c09e7cff8291b8e122574a282db741e404320a
Abrudan Andrei <andiabrudan@yahoo.com>
2019-01-19 22:19:13 +0200

    Fixed delete

--- src/webapp/src/pages/User Page/UserDeletePage/UserDeletePage.js
+++ src/webapp/src/pages/User Page/UserDeletePage/UserDeletePage.js
@@ -7,43 +7,21 @@ export default class UserDeletePage extends Component {
         super(props);
 
         this.state = {
-          username: '',
-          successMessage: null,
-          errorMessage: null,
+            username: "",
+            successMessage: null,
+            errorMessage: null,
         };
-
-        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(event) {
-      this.setState({ username: event.target.username });
+    handleChange = (event) => this.setState({ username: event.target.value });
 
-      console.log(this.state);
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log(this.state.username);
-        axios.delete(
-            axios.defaults.baseURL + `delete/${this.state.username}`
-        ).then(result => {
-            this.setState({
-                successMessage: true,
-            });
-        }).catch(err => {
-            this.setState({
-                errorMessage: true,
-            });
-        });
+    handleSubmit = () => {
+        axios.delete(`delete/${this.state.username}`)
+            .then(result => this.setState({ successMessage: true }))
+            .catch(err => this.setState({ errorMessage: true }));
     }
 
     render() {
-        // const user = this.props.session.user;
-        const user = {
-            userId: 2,
-            isAdmin: true,
-        };
-
         return (
             <div className="jumbotron user-delete-form">
                 {this.state.successMessage && <span className="badge badge-primary response-message">User deleted!</span>}
@@ -51,11 +29,11 @@ export default class UserDeletePage extends Component {
                 <form>
                     <div className="form-group">
                         <label>Username</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" value={this.state.username} aria-describedby="username" placeholder="Username"/>
+                        <input onChange={this.handleChange} className="form-control" type="text" value={this.state.username} aria-describedby="username" placeholder="Username" />
                         <small id="emailHelp" className="form-text text-muted">Please enter the username of the user you want to delete.</small>
                     </div>
-
-                    <input type="submit" className="btn btn-primary btn-submit" value="Delete" />
+                    {/* doesn't need preventDefault */}
+                    <input type="button" className="btn btn-primary btn-submit" value="Delete" onClick={this.handleSubmit} />
                 </form>
             </div>
         );
